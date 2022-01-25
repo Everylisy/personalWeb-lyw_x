72fd6d219191d558953dc74ce2dc586e47934090
violetwhenisawu <60170829+violetwhenisawu@users.noreply.github.com>
2022-01-25 22:52:19 +0900

    code update #1
    js 살짝 수정

--- main.js
+++ main.js
@@ -3,42 +3,63 @@ const content2 = document.querySelector(".content2");
 const content3 = document.querySelector(".content3");
 const content4 = document.querySelector(".content4");
 
-content1.addEventListener("mouseover", () => {
-    content1.innerText = "React & 인터랙티브 웹";
-  });
 
-content1.addEventListener("mouseout", () => {
-    content1.innerText = '# Interests';
-});
+//1번 컨텐츠
 
+function textChange1() {
+  content1.innerText = "React & 인터랙티브 웹";
+}
 
-content2.addEventListener("mouseover", () => {
+function textChange2() {
+  content1.innerText = '# Interests';
+}
+
+content1.addEventListener("mouseover", textChange1);
+content1.addEventListener("mouseout", textChange2);
+
+//2번 컨텐츠
+
+function textChange3() {
   content2.innerText = "성장하는 개발자가 되는 것.";
-});
-//   REACT와 인터랙티브 웹에 관심이 있어요!
-content2.addEventListener("mouseout", () => {
+}
+
+function textChange4() {
   content2.innerText = '# What to expect';
-});
+}
 
-content3.addEventListener("mouseover", () => {
+content2.addEventListener("mouseover", textChange3);
+content2.addEventListener("mouseout", textChange4);
+
+//3번 컨텐츠
+
+function textChange5() {
   content3.innerText = "우리 6개월간 열심히 해봐요 !";
-});
-//   REACT와 인터랙티브 웹에 관심이 있어요!
-content3.addEventListener("mouseout", () => {
+}
+
+function textChange6() {
   content3.innerText = '# To Racers';
-});
+}
 
-content4.addEventListener("mouseover", () => {
+content3.addEventListener("mouseover", textChange5);
+content3.addEventListener("mouseout", textChange6);
+
+//4번 컨텐츠
+
+function textChange7() {
   content4.innerText = "백예린 노래들, The Volunteers";
-});
-//   REACT와 인터랙티브 웹에 관심이 있어요!
-content4.addEventListener("mouseout", () => {
+}
+
+function textChange8() {
   content4.innerText = '# Things i love';
-});
+}
 
-var audio = new Audio('Late.mp3');
+content4.addEventListener("mouseover", textChange7);
+content4.addEventListener("mouseout", textChange8);
 
-/*종료되면 처음부터 다시 재생*/
+// 음악
+const audio = new Audio('Late.mp3');
+
+// 종료되면 다시 재생
 audio.addEventListener('ended', function() { 
     this.currentTime = 0;
     this.play();
